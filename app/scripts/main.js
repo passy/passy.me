b8d2838253c74d70126fbf6cf5f5e858411e3eff
Pascal Hartig <phartig@rdrei.net>
2013-03-31 01:07:35 +0100

    Load content dynamically, stretch footer to full size

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,24 @@
 /*global angular*/
 
-(function () {
+(function (window) {
     'use strict';
 
-    window.passy = angular.module('passy', []);
-}());
+    window.passy = angular.module('passy', [], function ($routeProvider,
+                                                         $locationProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'pages/index.html'
+        }).when('/projects.html', {
+            templateUrl: 'pages/projects.html',
+            controller: 'ProjectsCtrl'
+        });
+
+        // Static pages
+        ['changelog'].forEach(function (page) {
+            $routeProvider.when(['/', page, '.html'].join(''), {
+                templateUrl: ['pages/', page, '.html'].join('')
+            });
+        });
+
+        $locationProvider.html5Mode(true);
+    });
+}(this));
