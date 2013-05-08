f06b7535147f56cb1cd69afdecbbf11b46eecf59
Pascal Hartig <phartig@rdrei.net>
2013-05-08 23:28:22 +0200

    Cleaned up Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -137,17 +137,6 @@ module.exports = function (grunt) {
                 }
             }
         },
-        // not used since Uglify task does concat,
-        // but still available if needed
-        /*concat: {
-            dist: {}
-        },*/
-        // not enabled since usemin task does concat and uglify
-        // check index.html to edit your build targets
-        // enable this task if you prefer defining your build targets here
-        /*uglify: {
-            dist: {}
-        },*/
         rev: {
             dist: {
                 files: {
