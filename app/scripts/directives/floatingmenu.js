0e22e09c40f6486d568ff8d5e9cb204704021037
Pascal Hartig <phartig@rdrei.net>
2013-08-04 21:44:08 +0200

    Removed unused stuff

--- app/scripts/directives/floatingmenu.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    'use strict';
-
-    window.passy.directive('passyFloatingmenu', [function () {
-        return function link() {
-            // No-op for now, gotta hand-roll my own thingy.
-        };
-    }]);
-}());
