ba7dc38726f4b337af67e67069ea1017a5ef9348
Pascal Hartig <phartig@rdrei.net>
2013-08-17 23:17:53 +0200

    Added travis CI config

--- Gruntfile.js
+++ Gruntfile.js
@@ -124,7 +124,8 @@ module.exports = function (grunt) {
         karma: {
             unit: {
                 configFile: 'karma.conf.js',
-                singleRun: true
+                singleRun: true,
+                browsers: ['PhantomJS', 'Firefox']
             },
             server: {
                 configFile: 'karma.conf.js',
