f9c3cbbfc1b757d39d5253ffc170edb24625751e
Pascal Hartig <phartig@rdrei.net>
2013-08-18 01:01:33 +0200

    Remove ng-view for good

--- KNOWN_ISSUES.md
+++ KNOWN_ISSUES.md
@@ -1,10 +1,6 @@
 Known Issues
 ============
 
-* Page content is fetched twice, because I want the page to load w/o JS, but
-  Angular loads it on top. Possible solutions: Ditch ng-view completely as it
-  doesn't really add that much; implement a smarter ng-view that only replaces
-  content on demand.
 * Menu doesn't expand. Possible solution: Getting my lazy ass up and implement
   it.
 * Not "retina ready"
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,25 +2,5 @@
 
 (function (window) {
     'use strict';
-
-    window.passy = angular.module('passy', ['mgcrea.bootstrap.affix'],
-                                  ['$routeProvider', '$locationProvider', function (
-        $routeProvider, $locationProvider) {
-
-            $routeProvider.when('/', {
-                templateUrl: 'pages/index.html'
-            }).when('/projects.html', {
-                templateUrl: 'pages/projects.html'
-            });
-
-            // Static pages
-            ['impress', 'changelog'].forEach(function (page) {
-                $routeProvider.when(['/', page, '.html'].join(''), {
-                    templateUrl: ['pages/', page, '.html'].join('')
-                });
-            });
-
-            $locationProvider.html5Mode(true);
-        }
-    ]);
+    window.passy = angular.module('passy', ['mgcrea.bootstrap.affix']);
 }(this));
